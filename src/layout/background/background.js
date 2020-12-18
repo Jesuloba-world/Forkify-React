501ed003db1af478fd47b0de1d9e0410b6dcc215
Jesuloba <jesulobajohn@gmail.com>
2020-12-18 20:03:26 +0100

    Added Top component

--- src/layout/background/background.js
+++ src/layout/background/background.js
@@ -1,10 +1,12 @@
 import classes from "./background.module.css";
 import Logo from "../../components/Logo/Logo";
 import Footer from "../Footer/Footer";
+import Top from "./Top/Top";
 
 const background = (props) => {
 	return (
 		<div className={classes.Background}>
+			<Top />
 			<div className={classes.Logo_holder}>
 				<Logo />
 			</div>
