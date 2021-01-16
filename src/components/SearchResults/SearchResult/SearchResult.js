996b3d861f07b7cf141d0eb03c80a319a7c9798c
Jesuloba <jesulobajohn@gmail.com>
2021-01-16 07:58:58 +0100

    results are now well displayed

--- src/components/SearchResults/SearchResult/SearchResult.js
+++ src/components/SearchResults/SearchResult/SearchResult.js
@@ -1,22 +1,41 @@
+import classes from "./SearchResult.module.css";
+
 const SearchResult = (props) => {
-	const limitRecipeTitle = (title) => {
+	const limitRecipeTitle = (title, limit = 17) => {
+		const newTitle = [];
+		if (title.length > limit) {
+			title.split(" ").reduce((acc, curr) => {
+				if (acc + curr.length <= limit) {
+					newTitle.push(curr);
+				}
+				return acc + curr.length;
+			}, 0);
+
+			// return the result
+			return `${newTitle.join(" ")}...`;
+		}
 		return title;
 	};
 
 	return (
 		<li>
-			<a className="results__link" href={`#${props.result.recipe_id}`}>
-				<figure className="results__fig">
+			<a
+				className={classes.results__link}
+				href={`#${props.result.recipe_id}`}
+			>
+				<figure className={classes.results__fig}>
 					<img
 						src={props.result.image_url}
 						alt={props.result.title}
 					/>
 				</figure>
-				<div className="results__data">
-					<h4 className="results__name">
+				<div className={classes.results__data}>
+					<h4 className={classes.results__name}>
 						{limitRecipeTitle(props.result.title)}
 					</h4>
-					<p className="results__author">{props.result.publisher}</p>
+					<p className={classes.results__author}>
+						{props.result.publisher}
+					</p>
 				</div>
 			</a>
 		</li>
--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -7,6 +7,7 @@ import classes from "./SearchResults.module.css";
 const SearchResults = (props) => {
 	const searchResult = useSelector((state) => state.search.result);
 	const searching = useSelector((state) => state.search.searching);
+	const searchError = useSelector((state) => state.search.error);
 
 	let transformedResult = null;
 
@@ -14,9 +15,18 @@ const SearchResults = (props) => {
 		if (searching) {
 			transformedResult = <Spinner />;
 		} else {
-			transformedResult = (
-				<h2 className={classes.no_search}>Search for Something</h2>
-			);
+			if (searchError) {
+				transformedResult = (
+					<div className={classes.error}>
+						<h2 className="error">There was an error</h2>
+						<p>{searchError}</p>
+					</div>
+				);
+			} else {
+				transformedResult = (
+					<h2 className={classes.no_search}>Search for Something</h2>
+				);
+			}
 		}
 	} else {
 		transformedResult = (
